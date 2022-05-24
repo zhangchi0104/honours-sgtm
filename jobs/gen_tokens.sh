070d40acd06063b8b7a0b106e22e3c82b7f37e50
Alex Zhang <zhangchi0104@live.com>
2022-05-24 15:59:12 +0800

    updated args

--- jobs/gen_tokens.sh
+++ jobs/gen_tokens.sh
@@ -2,9 +2,9 @@
 #PBS -l select=1:ncpus=16:mem=32gb
 #PBS -l walltime=1:00:00
 #PBS -M chi.zhang@student.unsw.edu.au
-
+#PBS -m ae
 conda activate honours
-cd honours-sgtm
+cd $HOME/honours-sgtm
 python3 gen_tokens.py \
     --out  /srv/scratch/z5211214 \
     --pretrained
