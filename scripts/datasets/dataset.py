84c82b53988f551a9daa352e4ad72328ae6e44c1
Alex Zhang <zhangchi0104@live.com>
2022-09-08 09:08:11 +0800

    fixed problems

--- scripts/datasets/dataset.py
+++ scripts/datasets/dataset.py
@@ -50,9 +50,8 @@ class BertDataModule(pl.LightningDataModule):
         self.num_workers = num_workers
 
     def setup(self, stage=None):
-        self.train_dataset = BertDataset(self.train_data[:1000],
-                                         self.tokenizer)
-        self.val_dataset = BertDataset(self.val_data[:1000], self.tokenizer)
+        self.train_dataset = BertDataset(self.train_data, self.tokenizer)
+        self.val_dataset = BertDataset(self.val_data, self.tokenizer)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset,
--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@
 
 WANDB_API_KEY=625102dbe41c4af6314e1be904fe8102adfb1ca5 python3 scripts/train.py \
     --dataset_path=data/agnews/corpus/agnews_cleaned.txt \
-    --batch_size=20 \
+    --batch_size=6 \
     --accelerator=cuda \
     --max_epochs=10 \
     --output_dir=./models/agnews \
-    --num_workers=16 \
+    --num_workers=16
